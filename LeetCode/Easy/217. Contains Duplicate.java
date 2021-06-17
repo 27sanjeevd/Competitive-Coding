811aa28241e892e84da71bd13b07ef1a44bcbf03
Sanjeev Devarajan <62220999+27sanjeevd@users.noreply.github.com>
2021-06-16 20:34:58 -0500

    Create 217. Contains Duplicate.java

---
+++ LeetCode/Easy/217. Contains Duplicate.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for(int x = 0; x < nums.length - 1; x++){
+            if (nums[x] == nums[x + 1]){
+                return true;
+            }
+        }    
+        
+        return false;
+    }
+}
