a54a62ee8e8d9d54b29309add0499b7ca26b1a31
Sanjeev Devarajan <realsanjeevd@gmail.com>
2022-01-16 21:33:00 -0600

    Update 55. Jump Game.java

--- LeetCode/Medium/55. Jump Game.java
+++ LeetCode/Medium/55. Jump Game.java
@@ -1,21 +1,16 @@
 class Solution {
     public boolean canJump(int[] nums) {
         
-        int[] a1 = new int[nums.length];
-        
-        a1[0] = 1;
-        
-        for(int x = 0; x < nums.length - 1; x++){
-            if (a1[x] != 0){
-                for(int y = 1; y <= nums[x]; y++){
-                    if (x + y < a1.length){
-                        a1[x + y]++;
-                    }
-                }
+        int max = 0;
+        for(int x = 0; x < nums.length; x++){
+            if (x > max){
+                return false;
+            }
+            if (max < x + nums[x]){
+                max = x + nums[x];
             }
         }
         
-        return a1[a1.length - 1] > 0;
-        
+        return true;
     }
 }
