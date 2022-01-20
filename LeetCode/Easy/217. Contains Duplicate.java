0cac27cd18d7a0acc59058b1410f718744ca86c5
Sanjeev Devarajan <realsanjeevd@gmail.com>
2022-01-20 13:56:38 -0600

    Update 217. Contains Duplicate.java

--- LeetCode/Easy/217. Contains Duplicate.java
+++ LeetCode/Easy/217. Contains Duplicate.java
@@ -1,11 +1,20 @@
+/*
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+*/
+
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums);
-        for(int x = 0; x < nums.length - 1; x++){
-            if (nums[x] == nums[x + 1]){
+        HashMap<Integer, Integer> map1 = new HashMap<>();
+        
+        for(int x = 0; x < nums.length; x++){
+            if (map1.get(nums[x]) != null){
                 return true;
             }
-        }    
+            else{
+                map1.put(nums[x], 1);
+            }
+            
+        }
         
         return false;
     }
